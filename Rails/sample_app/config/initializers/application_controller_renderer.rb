e4b06ea2fb18269ca992e2068e4e5e23ae1e1812
atu <61017515+3flower@users.noreply.github.com>
2020-04-07 15:51:54 +0900

    [Finish}

---
+++ Rails/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '969864b39445ec743ee756a259ceed8e77853f6adbff02a02bd2e16a7df61dacf991f4ab45d8ceabd452803bb3cd785dda4ef8e6073dd3256463e696d6249cd6'
