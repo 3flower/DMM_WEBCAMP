2a849c9bda2ed4f5417c68da9376b3168c65a6a3
atu <61017515+3flower@users.noreply.github.com>
2020-04-09 19:17:31 +0900

    commit

---
+++ Rails/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '33061cf5a7cc85b2488670f217e99ca2b9b79888aedc3706631885a01e54cebf74bc91a9e3d26ebaf69457832ab585ed247c1ea23db66791e3fcd83313ad2a0b'
